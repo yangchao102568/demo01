4eed87355cf65489c8b9df81fbf09a11ab61eeaf
yangchao102568 <319981910@qq.com>
2018-10-22 08:38:19 +0800

    星期一

--- src/com/lx/cc/demo01.java
+++ src/com/lx/cc/demo01.java
@@ -1,10 +1,11 @@
 package com.lx.cc;
 
 public class demo01 {
-	
+
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println("I love you and you love me");
+
+		System.out.println("今天星期一");
 	}
 }
